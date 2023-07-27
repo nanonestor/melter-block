d041d9d5e586571545bc4d5e5dc231284ff1f396
nanonestor <68798571+nanonestor@users.noreply.github.com>
2023-07-27 01:12:03 -0700

    1.3.3-1.19.2 update
    -Restored KubeJS support!
    -Added Minecraft Soul Torches as heat source

--- src/main/java/com/oierbravo/melter/compat/kubejs/KubeJSPlugin.java
+++ src/main/java/com/oierbravo/melter/compat/kubejs/KubeJSPlugin.java
@@ -1,13 +1,13 @@
-/*
+
 package com.oierbravo.melter.compat.kubejs;
 
-import dev.latvian.mods.kubejs.recipe.RegisterRecipeTypesEvent;
-import net.minecraft.resources.ResourceLocation;
+import com.oierbravo.melter.content.melter.MeltingRecipe;
+import dev.latvian.mods.kubejs.recipe.schema.RegisterRecipeSchemasEvent;
 
 public class KubeJSPlugin extends dev.latvian.mods.kubejs.KubeJSPlugin {
-    public void registerRecipeTypes(RegisterRecipeTypesEvent event) {
-        event.register(new ResourceLocation("melter:melting"),  MeltingRecipeProcessing::new);
+    @Override
+    public void registerRecipeSchemas(RegisterRecipeSchemasEvent event) {
+        event.register(MeltingRecipe.Serializer.ID, MeltingRecipeSchema.SCHEMA);
     }
 
 }
-*/
